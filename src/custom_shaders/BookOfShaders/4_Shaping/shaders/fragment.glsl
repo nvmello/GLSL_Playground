296a3d0c1b36849610d12f8dc475337d16bce42d
Nick <nickvmorello@gmail.com>
2025-01-14 00:40:34 -0700

    step interpolation, smoothstep, and other shaping functions

--- src/custom_shaders/BookOfShaders/4_Shaping/shaders/fragment.glsl
+++ src/custom_shaders/BookOfShaders/4_Shaping/shaders/fragment.glsl
@@ -7,20 +7,46 @@ precision mediump float;
 uniform vec2 u_resolution;
 uniform float u_time;
 
-// Plot a line on Y using a value between 0.0-1.0
+/**
+ Plot a line on Y using a value between 0.0-1.0
+ Takes a 2D coordinate (st)
+ calculates how far a point is from the y=x diagonal line, creates a smooth transition
+ Returns 1.0 when distance is 0.0 
+ Returns 0.0 when distance is 0.01 or greater
+ */
 float plot(vec2 st, float pct) {    
     return smoothstep(pct - 0.01, pct, st.y) - smoothstep(pct, pct + 0.01, st.y);
 }
 
-void main() {
+// void main() {
+// 	vec2 st = gl_FragCoord.xy/u_resolution; //gl_FragCoord.xy gives the normalized pixel coordinates
+
+//     float y = pow(st.x,8.0); //Takes the x coordinate (st.x, which is normalized between 0 and 1). Raises it to the power of 8.0
+	
+//     vec3 color = vec3(y); //Creates a grayscale gradient from black (0,0,0) to white (1,1,1)
+
+// 	float pct = plot(st, y); //pct is 1.0 on the line, 0.0 elsewhere
+
+// 	color = (1.0-pct)*color+pct*vec3(0.0,1.0,0.0); //keeps the background gradient where pct is 0, adds green where pct is 1
+
+// 	gl_FragColor = vec4(color,1.0); //Sets the final pixel color. Converts vec3 color to vec4 by adding 1.0 alpha (fully opaque)
+// }
+
+void main(){
 	vec2 st = gl_FragCoord.xy/u_resolution;
 
-    float y = pow(st.x,8.0);
+	// Step will return 0.0 unless the value is over 0.5,
+    // in that case it will return 1.0
+    // float y = step(0.5,st.x);
 
-    vec3 color = vec3(y);
+	// Smooth interpolation between 0.1 and 0.9
+	// float y = smoothstep(0.1,0.9,st.x);
+	float y = smoothstep(0.2,0.5,st.x) - smoothstep(0.5,0.8,st.x);
 
-	float pct = plot(st, y);
-	color = (1.0-pct)*color+pct*vec3(0.0,1.0,0.0);
+	vec3 color = vec3(y);
 
-	gl_FragColor = vec4(color,1.0);
+    float pct = plot(st,y);
+    color = (1.0-pct)*color+pct*vec3(0.0,1.0,0.0);
+
+    gl_FragColor = vec4(color,1.0);
 }
